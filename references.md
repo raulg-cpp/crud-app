b4877f96c4548377d45256cf56e2656dbb804694
raulg-cpp <raulg.cpp@gmail.com>
2023-06-07 15:12:10 -0400

    add data to firebase, outline

--- references.md
+++
@@ -1,4 +0,0 @@
-# References
-
-## Github pages
-https://blog.logrocket.com/deploying-react-apps-github-pages/
---
+++ src/handle-submit.js
@@ -0,0 +1,18 @@
+import { addDoc, collection } from "@firebase/firestore"
+import { db } from "./firebase-config"
+ 
+const handleSubmit = (testdata) => {
+    const ref = collection(db, "data") // Firebase creates this automatically
+ 
+    let data = {
+        testData: testdata
+    }
+    
+    try {
+        addDoc(ref, data)
+    } catch(err) {
+        console.log(err)
+    }
+}
+ 
+export default handleSubmit
